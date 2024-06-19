e2b7abc36e168051593c7acb48c4aaf73a70fa55
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-06-19 18:07:51 +0530

    "Updated .gitignore to include .env file and replaced hardcoded API key in script.js with environment variable"

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const currency = document.querySelector("#currencySelect");
 const convertbtn = document.querySelector("#convertbtn");
 const result = document.querySelector("#displayResult");
 
-const API_KEY = "fGqtg1X2CZRDxYwBsS2NOpg==e950VoZXGOML6SuO";
+const API_KEY = `${process.env.API_KEY}`;
 const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?";
 
 convertbtn.addEventListener("click", () => {
