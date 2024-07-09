d12c288b1abefd49e7e293f07a5ff24bfaa4ee4f
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-07-09 23:56:03 +0530

    "Added stylesheet, refactored HTML structure, and updated JavaScript code for currency converter"

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const { response } = require("express");
+require("dotenv").config();
 
-const amount = document.querySelector("#inputNumber");
-const currency = document.querySelector("#currencySelect");
-const convertbtn = document.querySelector("#convertbtn");
-const result = document.querySelector("#displayResult");
+document.addEventListener("DOMContentLoaded", function () {
+  const amount = document.querySelector("#inputNumber");
+  const currency = document.querySelector("#currencySelect");
+  const convertbtn = document.querySelector("#convertbtn");
+  const result = document.querySelector("#displayResult");
 
-const API_KEY = `${process.env.API_KEY}`;
-const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?";
+  const API_KEY = `${process.env.API_KEY}`;
+  const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?pair=GBP_AUD";
 
-convertbtn.addEventListener("click", () => {
-  const amountTotal = amount.value;
-  const currencyTotal = currency.value;
-  const url = apiUrl + currencyTotal;
+  convertbtn.addEventListener("click", () => {
+    const amountTotal = amount.value;
+    console.log("total", amountTotal);
+    const currencyTotal = currency.value;
+    const url = apiUrl + currencyTotal;
 
-  fetch(url, {
-    headers: {
-      "X-API-KEY": API_KEY,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const rate = data.rate;
-      result = amount * rate;
-      result.innerHTML = `${amount} ${currency} = ${result.toFixed(2)} USD`;
+    fetch(url, {
+      headers: {
+        "X-API-KEY": API_KEY,
+      },
     })
-    .catch((error) => {
-      console.error("Request failed:", error);
-      result.innerHTML = "an error occured please try again ";
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        const rate = data.exchange_rate;
+        console.log("rate", rate);
+        const resultPrice = amount * rate;
+        console.log("result", resultPrice);
+        result.innerHTML = `${amount} ${currency} = ${resultPrice.toFixed(
+          2
+        )} USD`;
+      })
+      .catch((error) => {
+        console.error("Request failed:", error);
+        result.innerHTML = "an error occured please try again ";
+      });
+  });
 });
